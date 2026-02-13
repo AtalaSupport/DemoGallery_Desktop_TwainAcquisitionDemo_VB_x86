fe73d370dc6a95c8f0b8db5cb1e322d2a7ef0af8
Atalasoft Support <developersupport@atalasoft.com>
2026-02-13 18:40:39 -0500

    initial commit

---
+++ Acquisition/AboutDialog.vb
@@ -0,0 +1,28 @@
+﻿Public Class AboutDialog
+
+    Public Sub New()
+
+        ' This call is required by the Windows Form Designer.
+        InitializeComponent()
+
+        Dim asm As System.Reflection.Assembly = System.Reflection.Assembly.Load("Atalasoft.DotTwain")
+        Me.lblVersion.Text = "Version: " & asm.GetName().Version.ToString()
+    End Sub
+
+
+    Private Sub linkLabel1_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles linkLabel1.LinkClicked
+        System.Diagnostics.Process.Start("https://www.atalasoft.com")
+    End Sub
+
+    Private Sub apiReferenceLinkLabel_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles downloadHelpLinkLabel.LinkClicked
+        System.Diagnostics.Process.Start("https://www.atalasoft.com/Support/APIs-Dev-Guides")
+    End Sub
+
+    Private Sub demoGalleryLinkLabel_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles demoGalleryLinkLabel.LinkClicked
+        System.Diagnostics.Process.Start("www.atalasoft.com/Support/Sample-Applications")
+    End Sub
+
+    Private Sub downloadDotImageLinkLabel_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles downloadDotImageLinkLabel.LinkClicked
+        System.Diagnostics.Process.Start("https://www.atalasoft.com/BeginDownload")
+    End Sub
+End Class
---
+++ README.md
@@ -0,0 +1,25 @@
+# TwainAcquisitionDemo (Twain Scanning)
+This is the main demo for our DotTwain Acquisition 
+
+This is the VB.NET version
+
+NOTE that there is no x64 version. This is due to most TWAIN drivers being native 32 bit.
+
+Please see [FAQ: Common Problems with x64 TWAIN Scanning](https://www.atalasoft.com/kb2/KB/50140/FAQ-Common-Problems-with-x64-TWAIN-Scanning)
+
+## Prerequisites
+This demo assumes you have the Atalasoft DotImage SDK installed, but you only need
+licsning for DotTwain (Or DOtImage Document Imaging)
+
+You may also request a 30 day evaluation when installing / activating.
+
+[Download DotImage](https://www.atalasoft.com/BeginDownload/DotImageDownloadPage)
+
+## Cloning
+We recommend the following to ensure you clone with the required submodule
+
+Example: git for windows
+```bash
+git clone https://github.com/AtalaSupport/DemoGallery_Desktop_TwainAcquisitionDemo_VB_x86.git TwainAcquisitionDemo
+```
+
